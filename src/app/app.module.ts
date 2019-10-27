5c433e0054a89353c53112ad22600f14b3ea5b37
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2019-10-27 16:32:49 +0100

    Add firebase as environment and config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCORUBxnMeMmqVuujh6OSvJ3bq0YvtTDtE',
+    authDomain: 'profile-image-creator.firebaseapp.com',
+    databaseURL: 'https://profile-image-creator.firebaseio.com',
+    projectId: 'profile-image-creator',
+    storageBucket: 'profile-image-creator.appspot.com',
+    messagingSenderId: '919162423289',
+    appId: '1:919162423289:web:10effec148140a07441f28',
+    measurementId: 'G-36B53REMEC'
+  }
 };
 
 /*
