1cae5eeb68252e7d55774361b9d82648b6a9e752
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2019-11-27 08:04:20 +0100

    Working on canvas

--- src/app/features/image-editor/pages/image-editor-edit/image-editor-edit.component.ts
+++ src/app/features/image-editor/pages/image-editor-edit/image-editor-edit.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: "app-image-editor-edit",
   templateUrl: "./image-editor-edit.component.html",
   styleUrls: ["./image-editor-edit.component.scss"]
 })
-export class ImageEditorEditComponent implements OnInit {
+export class ImageEditorEditComponent implements OnInit, AfterViewInit {
+  name = "Angular 8 by Example: ElementRef";
+  @ViewChild("imageElm", { static: false }) imageElm: ElementRef;
+
+  //   var c = document.getElementById("myCanvas");
+  // var ctx = c.getContext("2d");
+  // ctx.beginPath();
+  // ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+  // ctx.stroke();
+
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    let imgDiv = this.imageElm.nativeElement.src;
+    debugger;
+  }
 }
