5a7f88d010d733d5c0517e0a30a65db21c112901
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-03-10 12:25:15 +0100

    Optimize upload image page layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedFooterComponent } from './components/shared-footer/shared-footer.component';
 import { SharedHeaderComponent } from './components/shared-header/shared-header.component';
 
 @NgModule({
-  declarations: [SharedHeaderComponent],
+  declarations: [SharedHeaderComponent, SharedFooterComponent],
   imports: [CommonModule],
-  exports: [SharedHeaderComponent]
+  exports: [SharedHeaderComponent, SharedFooterComponent]
 })
 export class SharedModule {}
