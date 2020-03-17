260ee02ee0097d1245d179396057d49b5045799c
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-03-17 12:07:48 +0100

    To enable cron jobs, must be on blaze plan, pay as you go on firebase, which i dont want to risk that i made an error and suddently get a thousand dollar bill

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,8 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
+
+// export const scheduledFunction = functions.pubsub.schedule('every 5 minutes').onRun(context => {
+//   console.log('This will be run every 5 minutes!');
+//   return null;
+// });
