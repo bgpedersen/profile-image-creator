d952c0ceceb7e8eb9ecb4cfb920aeb3cd7f86295
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-03-26 21:37:05 +0100

    Working on onDownload test

--- src/app/features/image-editor/components/edit-image/edit-image.component.spec.ts
+++ src/app/features/image-editor/components/edit-image/edit-image.component.spec.ts
@@ -4,23 +4,34 @@ import { AngularFireStorage } from '@angular/fire/storage';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MatDialog } from '@angular/material/dialog';
 
+import { ImageEditorService } from '../../services/image-editor.service';
 import { EditImageComponent } from './edit-image.component';
 
-class ImageEditorService {}
+// class ImageEditorServiceMock extends ImageEditorService {
+// canvasToBlob: (canvas: )=>{}
+//   upload: () => {};
+//   retryRetrieveDownloadUrls: () => {};
+// }
 
-describe('EditImageComponent', () => {
+fdescribe('EditImageComponent', () => {
   let component: EditImageComponent;
   let fixture: ComponentFixture<EditImageComponent>;
-
-  const imageEditorService = new ImageEditorService();
-  spyOnAllFunctions(imageEditorService);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [EditImageComponent],
       providers: [
-        { provide: ImageEditorService, useValue: imageEditorService },
-        { provide: MatBottomSheet, useValue: {} },
+        {
+          provide: ImageEditorService,
+          useValue: jasmine.createSpyObj([
+            'canvasToBlob',
+            'upload',
+            'retryRetrieveDownloadUrls',
+            'createImageFromImageDataUrl',
+            'canvasDraw'
+          ])
+        },
+        { provide: MatBottomSheet, useValue: jasmine.createSpyObj(['open']) },
         { provide: MatDialog, useValue: {} },
         { provide: AngularFireStorage, useValue: {} }
       ],
@@ -31,10 +42,38 @@ describe('EditImageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EditImageComponent);
     component = fixture.componentInstance;
+    // spyOnAllFunctions(TestBed.inject(ImageEditorService));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onDownload', () => {
+    it('should call openBottomSheet on success', () => {
+      component.openBottomSheet = jasmine.createSpy('openBottomSheet');
+      // const spy = spyOn(component, 'openBottomSheet').and.returnValue();
+      // const imageEditorService = TestBed.inject(ImageEditorService);
+      // jasmine.createSpy(component.ma, 'open');
+      // spyOn(imageEditorService, 'upload').and.resolveTo();
+      // spyOnAllFunctions(imageEditorService);
+      // imageEditorService.canvasToBlob = jasmine.createSpy('canvasToBlob').and.resolveTo({} as Blob);
+      // imageEditorService.upload = jasmine.createSpy('upload').and.resolveTo('id');
+      // imageEditorService.retryRetrieveDownloadUrls = jasmine.createSpy('retryRetrieveDownloadUrls').and.resolveTo([]);
+      component.onDownload();
+      // expect(component.loading).toBe(false);
+      // expect(component.openBottomSheet).toHaveBeenCalled();
+    });
+
+    xit('should call matDialog.open on error', () => {
+      pending();
+      const matDialogSpy = spyOn(component.matDialog, 'open');
+
+      component.onDownload();
+
+      expect(matDialogSpy).toHaveBeenCalled();
+    });
   });
 });
